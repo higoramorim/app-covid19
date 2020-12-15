0af2dc5b0449e494c2cd2b9bd82e3c40c62b06d0
higoramorim <1810042@aluno.univesp.br>
2020-12-15 08:40:12 -0300

    add

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,41 @@
+import BrazilFlag from '../../assets/images/brasil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/alemanha.png'
+import ItalyFlag from '../../assets/images/italia.png'
+import UsaFlag from '../../assets/images/estadosunidos.png'
+import SweedenFlag from '../../assets/images/suecia.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Suécia',
+    value: 'sweeden',
+    flag: SweedenFlag
+  },
+]
+
+export default COUNTRIES
